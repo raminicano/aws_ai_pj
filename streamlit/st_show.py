eedd283edfa72453218ea16effc00326f3d5b397
raminicano <raminicayes@gmail.com>
2024-06-24 08:34:03 +0000

    st show

--- streamlit/st_show.py
+++ streamlit/st_show.py
@@ -42,13 +42,13 @@ def summarize_PDF_file(pdf_file, model, input_title):
             split_texts = text_splitter.split_text(texts)
             st.write(len(split_texts), "단락이 있음.")
             
-            # 결과 출력
-            st.write("단락별 텍스트:")
-            for i, paragraph in enumerate(split_texts):
-                st.write(f"단락 {i+1}:")
-                st.write(paragraph)
-                st.write("---")
-        if model == "hf":
+            # # 결과 출력
+            # st.write("단락별 텍스트:")
+            # for i, paragraph in enumerate(split_texts):
+            #     st.write(f"단락 {i+1}:")
+            #     st.write(paragraph)
+            #     st.write("---")
+        if model == "bart":
             url2 = f"{FASTAPI_URL2}/summarize"
             headers2 = {"Content-Type": "application/json", "titles": input_title}
             summaries = requests.post(url2, json={"texts": split_texts}, headers=headers2)
@@ -58,7 +58,7 @@ def summarize_PDF_file(pdf_file, model, input_title):
                 st.write(f"단락 {i+1}:")
                 st.write(summary)
                 st.write("---")
-        else:
+        elif model == "flan":
             print("구현중")
     else:
         st.write("PDF 파일를 업로드하세요.")
@@ -77,14 +77,14 @@ st.write("요약 모델을 선택하세요.")
 input_title = st.text_input("Paper title")
 st.write("논문의 제목을 입력하세요.")
 
-radio_selected_model = st.radio("PDF 문서 요약 모델", ["OpenAI","HuggingFace"], index=1, horizontal=True)
+radio_selected_model = st.radio("PDF 문서 요약 모델", ["flan-t5-3b-summarizer","bart-large-cnn"], index=1, horizontal=True)
 
 upload_file = st.file_uploader("PDF 파일를 업로드하세요.", type="pdf")
 
-if radio_selected_model == "HuggingFace":
-    model = "hf"
-else:
-    st.write("OpenAI 구상 중")
+if radio_selected_model == "flan-t5-3b-summarizer":
+    model = "flan"
+elif radio_selected_model == "bart-large-cnn":
+    model = "bart"
 
 clicked_sum_model = st.button('PDF 문서 요약')
 
